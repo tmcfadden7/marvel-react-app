fcd8d9049717c9d2484f2f65e6016b070bc25d82
Thomas McFadden <tmac727@yahoo.com>
2021-12-27 11:20:11 -0500

    Add CharacterGrid.js, import react hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import './App.css';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <div>
       <Header />
