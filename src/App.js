84fde60be8b3e34d8be770e0e5afce4e240aed80
Thomas McFadden <tmac727@yahoo.com>
2022-05-25 21:07:25 -0400

    WIP filter out characters with empty description

--- src/App.js
+++ src/App.js
@@ -2,36 +2,39 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from './Header';
 import Pagination from './components/Pagination';
-import CharacterGrid from './characters/CharacterGrid'
+import CharacterGrid from './characters/CharacterGrid';
 import './App.css';
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [name, setName] = useState('a');
+	const [characters, setCharacters] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [name, setName] = useState('a');
 
-  function getName(letter) {
-    setName(letter)
-    // console.log('click from getname', letter)
-  }
+	function getName(letter) {
+		setName(letter);
+		// console.log('click from getname', letter)
+	}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&limit=100&ts=1&apikey=381b1b1d55431234af33e3c11953547e&hash=1dcf741e1f53611062f293df3dfd240c`);
-      const data = await response.data.data.results;
-      // console.log(data);
-      setCharacters(data);
-      setIsLoading(false);
-    }
-    fetchData();
-  }, [name])
-  return (
-    <div>
-      <Header getName={getName}/>
-      <Pagination getName={getName}/>
-      <CharacterGrid characters={characters} isLoading={isLoading}/>
-    </div>
-  );
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await axios(
+				`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&limit=100&ts=1&apikey=381b1b1d55431234af33e3c11953547e&hash=1dcf741e1f53611062f293df3dfd240c`
+			);
+			const data = await response.data.data.results;
+			const filterData = await data.filter((char) => char.description !== '');
+			// console.log(filterData);
+			setCharacters(filterData);
+			setIsLoading(false);
+		};
+		fetchData();
+	}, [name]);
+	return (
+		<div>
+			<Header getName={getName} />
+			<Pagination getName={getName} />
+			<CharacterGrid characters={characters} isLoading={isLoading} />
+		</div>
+	);
 }
 
 export default App;
--- src/characters/CharacterGrid.js
+++ src/characters/CharacterGrid.js
@@ -1,21 +1,21 @@
-import Character from "./Character"
+import Character from './Character';
 
 const CharacterGrid = ({ characters, isLoading }) => {
-    return (
-        <>
-            <div className="container">
-            {isLoading ? <h2>Loading...</h2> :
-                <div className="row">
-                    {characters.map((character) => {
-                        return (
-                            <Character {...character} />
-                        )
-                    })}
-                </div>
-                }   
-            </div>
-        </>
-    )
-}
+	return (
+		<>
+			<div className='container mt-4'>
+				{isLoading ? (
+					<h2>Loading...</h2>
+				) : (
+					<div className='row'>
+						{characters.map((character) => {
+							return <Character {...character} />;
+						})}
+					</div>
+				)}
+			</div>
+		</>
+	);
+};
 
-export default CharacterGrid
+export default CharacterGrid;
