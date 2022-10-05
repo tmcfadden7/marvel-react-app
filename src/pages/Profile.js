3306780bf996bab76a05e390b1120e49bd96dbe8
Thomas McFadden <tmac727@yahoo.com>
2022-10-04 20:36:26 -0400

    create profile.js with placeholder text, add route for profile

---
+++ src/pages/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<section className='profile-container'>
+			<div className='container'>
+				<div className='profile'>
+					<div className='avatar'>IMAGE</div>
+					<div className='name'>
+						<p>NAME</p>
+					</div>
+					<div className='about'>
+						<p>ABOUT ME</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Profile;
