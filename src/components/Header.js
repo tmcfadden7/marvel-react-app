fabb6c448bbb028fc0e4048d348e6661218a2ddb
Thomas McFadden <tmac727@yahoo.com>
2022-10-08 22:07:34 -0400

    create navbar component, remove nav from header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import SignUp from './SignUp';
-import logo from '../assets/marvel-logo.jpg';
 import SignIn from './SignIn';
 
-const Header = ({ getName }) => {
+const Header = () => {
 	return (
 		<>
-			<nav className='navbar sticky-top navbar-expand-lg navbar-light'>
-				<div className='container'>
-					<Link to='/' className='navbar-brand'>
-						<img src={logo} alt='' className='d-inline-block align-text-top' />
-					</Link>
-					<button
-						className='navbar-toggler text-light bg-light'
-						type='button'
-						data-bs-toggle='collapse'
-						data-bs-target='#navbarSupportedContent'
-						aria-controls='navbarSupportedContent'
-						aria-expanded='false'
-						aria-label='Toggle navigation'
-					>
-						<span className='navbar-toggler-icon'></span>
-					</button>
-					<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-						<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-							<li className='nav-item'>
-								<Link to='/' className='nav-link active text-light'>
-									Home
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link to='/characters' className='nav-link text-light'>
-									Characters
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link to='/comics' className='nav-link text-light'>
-									Comics
-								</Link>
-							</li>
-						</ul>
-						<form className='d-flex'>
-							<input
-								className='form-control me-2 border border-danger border-3'
-								type='search'
-								placeholder='Search Character'
-								onKeyUp={(e) => {
-									e.target.value ? getName(e.target.value) : getName('a');
-								}}
-							/>
-							{/* <button className="btn btn-outline-danger" type="submit">Search</button> */}
-						</form>
-					</div>
-				</div>
-			</nav>
 			<section className='showcase-container'>
 				<div className='container'>
 					<div className='row'>
