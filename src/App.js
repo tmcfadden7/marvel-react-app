aae6cce536c9c71bd8ef830945e67d1b12b7086a
Thomas McFadden <tmac727@yahoo.com>
2022-10-11 18:49:01 -0400

    add image, style page

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
 	return (
 		<Router>
 			<NavBar />
-			<Header />
+			{/* <Header /> */}
 			<Routes>
 				<Route
 					path='/'
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -59,49 +59,51 @@ const SignUp = () => {
 		}
 	};
 	return (
-		<form onSubmit={onSubmit}>
-			<div className='mb-3'>
-				<label htmlFor='Name' className='form-label'>
-					Name
-				</label>
-				<input
-					type='text'
-					className='form-control'
-					id='name'
-					value={name}
-					onChange={onChange}
-				/>
-			</div>
-			<div className='mb-3'>
-				<label htmlFor='Email' className='form-label'>
-					Email
-				</label>
-				<input
-					type='email'
-					className='form-control'
-					id='email'
-					value={email}
-					onChange={onChange}
-				/>
-				<div id='emailHelp' className='form-text'>
-					We'll never share your email with anyone else.
-				</div>
-			</div>
+		<section className='sign-up-container'>
+			<div className='grid'>
+				<div className='sign-up-form bg-white text-dark m-4 p-5'>
+					<h1 className='h3'>Create a free account</h1>
+					<form onSubmit={onSubmit}>
+						<div className='mb-3'>
+							<input
+								type='text'
+								className='form-control'
+								id='name'
+								placeholder='Name'
+								value={name}
+								onChange={onChange}
+							/>
+						</div>
+						<div className='mb-3'>
+							<input
+								type='email'
+								className='form-control'
+								id='email'
+								placeholder='Email'
+								value={email}
+								onChange={onChange}
+							/>
+							<div className='emailHelp'>
+								We'll never share your email with anyone else.
+							</div>
+						</div>
 
-			<div className='mb-3'>
-				<label htmlFor='Password' className='form-label'>
-					Password
-				</label>
-				<input
-					type='password'
-					className='form-control'
-					id='password'
-					value={password}
-					onChange={onChange}
-				/>
+						<div className='mb-3'>
+							<input
+								type='password'
+								className='form-control'
+								id='password'
+								placeholder='Password'
+								value={password}
+								onChange={onChange}
+							/>
+						</div>
+						<button className='btn btn-primary'>Submit</button>
+					</form>
+				</div>
+				<div className='sign-up-img'></div>
 			</div>
-			<button className='btn btn-primary'>Submit</button>
-		</form>
+		</section>
 	);
 };
 
