78536e4fecc5a1d479a8273cb7f2452fd285a5c4
Thomas McFadden <tmac727@yahoo.com>
2023-04-09 20:54:53 -0400

    Create footer, add react icons package, add copy and sign up button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import SignIn from './SignIn';
+import { BsInstagram, BsFacebook, BsTwitter } from 'react-icons/bs';
 
 const Footer = () => {
 	const [show, setShow] = useState(false);
@@ -8,32 +9,54 @@ const Footer = () => {
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 	return (
-		<section className='footer p-5'>
+		<section className='footer py-4'>
 			<div className='container'>
-				<div className='row d-flex align-items-center'>
-					<div className='col-6 text-center'>
-						<Button
-							className='btn-outline-light text-white'
-							variant='light'
-							style={{ background: '#e62429' }}
-							onClick={handleShow}
+				<div className='d-flex flex-column justify-content-center text-center '>
+					<Button
+						className='btn-outline-dark text-dark mx-auto mb-3'
+						variant='light'
+						style={{ background: '#e62429' }}
+						onClick={handleShow}
+					>
+						<p className='mb-0'>Sign in/sign up</p>
+					</Button>
+					<Modal show={show} onHide={handleClose}>
+						<Modal.Body className='px-0 px-sm-3'>
+							<SignIn />
+						</Modal.Body>
+						<Modal.Footer>
+							<Button variant='secondary' onClick={handleClose}>
+								Close
+							</Button>
+						</Modal.Footer>
+					</Modal>
+					<div className='mb-3'>
+						<a
+							href='https://www.instagram.com/marvel/?hl=en'
+							target='_blank'
+							rel='noreferrer'
+							className='text-decoration-none text-dark me-3'
 						>
-							Sign in/sign up
-						</Button>
-						<Modal show={show} onHide={handleClose}>
-							<Modal.Body className='px-0 px-sm-3'>
-								<SignIn />
-							</Modal.Body>
-							<Modal.Footer>
-								<Button variant='secondary' onClick={handleClose}>
-									Close
-								</Button>
-							</Modal.Footer>
-						</Modal>
+							<BsInstagram />
+						</a>
+						<a
+							href='https://www.facebook.com/Marvel/'
+							target='_blank'
+							rel='noreferrer'
+							className='text-decoration-none text-dark me-3'
+						>
+							<BsFacebook />
+						</a>
+						<a
+							href='https://twitter.com/Marvel'
+							target='_blank'
+							rel='noreferrer'
+							className='text-decoration-none text-dark'
+						>
+							<BsTwitter />
+						</a>
 					</div>
-					<div className='col-6 text-center'>
-						<p className='mb-0'>&#169;{new Date().getFullYear()} Marvel</p>
-					</div>
+					<p className='mb-0'>&#169;{new Date().getFullYear()} Marvel</p>
 				</div>
 			</div>
 		</section>
